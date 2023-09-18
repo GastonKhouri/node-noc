0045fa10b042916c98418e50c8ca6a399ae1bb30
Gaston Khouri <gastonlozano2016@yahoo.com>
2023-09-18 16:52:23 -0400

    Clean Architecture - Repository Pattern

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,11 @@
 import { CheckService } from '../domain/use-cases/checks/check-service';
+import { FileSystemDatasource } from '../infrastructure/datasources/file-system.datasource';
+import { LogRepositoryImpl } from '../infrastructure/repositories/log.repository.empl';
 import { CronService } from './cron/cron-service';
+
+const fileSystemLogRepository = new LogRepositoryImpl(
+	new FileSystemDatasource()
+);
 
 export class Server {
 
@@ -12,6 +18,7 @@ export class Server {
 			() => {
 				const url = 'https://www.google.com';
 				new CheckService(
+					fileSystemLogRepository,
 					() => console.log( `${ url } is ok` ),
 					( error ) => console.log( error )
 				).execute( url );
